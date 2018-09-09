fda20dfa77af7fc55fbd97ec3770698324737df1
BanLister <42670510+BanLister@users.noreply.github.com>
2018-09-09 16:31:55 +1000

    Minor Improvements
    Removed debugging code.
    Added more cami support - rather than just checking CAMI isAdmin

--- Gmod-Community-BanLister/lua/autorun/server/sv_banlister.lua
+++ Gmod-Community-BanLister/lua/autorun/server/sv_banlister.lua
@@ -5,6 +5,7 @@ BanLister.MaxBans = 15 -- If the user has equal to or more recorded bans then th
 BanLister.RangeBans = "month" -- "total" - "month" -- MaxBans Time period.
 BanLister.KickHim = false -- if false, instead of kicking users for MaxBans a message will be sent to staff. If true a message won't be sent and the user will be kicked.
 BanLister.KickReason = "Sorry %s but this server is protected by Ban Lister ensuring a safe community" -- %s = player name
+BanLister.allowedAdmins = {["moderator"] = true, ["mod"] = true} -- alert other ranks that may now be classed as Admin with CAMI admin mods.
 BanLister.AdminMessage = "%s has %d bans in the past month recorded" -- %s = player name
 
 --DO NOT MODIFY PAST THIS POINT--
@@ -20,10 +21,10 @@ function BanLister:BanHandler(steamid64, reason, length)
 			game_id = "4000" -- gmod id
 		},
 		function(result)
-			print(result)
+			--print(result)
 		end, 
 		function(e)
-			print("[BanLister] API FAILED WITH STATUS: "..e)
+			--print("[BanLister] API FAILED WITH STATUS: "..e)
 		end
 	)
 end
@@ -38,11 +39,11 @@ hook.Add("PlayerInitialSpawn", "BanLister.CheckForBans", function(ply)
 		function(body, size, h, code)
 			local data = util.JSONToTable(body)
 
-			local count = table.Count(data)
+			local count = #data -- use the length operator when its numerically indexed (i believe in this case this is). table.Count is good for sequentially
 
-			PrintTable(data)
+			--PrintTable(data)
 
-			print("Bans count: "..count)
+			--print("Bans count: "..count)
 
 			if count >= BanLister.MaxBans then return end
 
@@ -51,14 +52,14 @@ hook.Add("PlayerInitialSpawn", "BanLister.CheckForBans", function(ply)
 				ply:Kick(string.format(BanLister.KickReason, name))
 			else
 				for k,v in pairs(player.GetAll()) do
-					if not v:IsAdmin() then continue end
+					if not v:IsAdmin() || not BanLister.allowedAdmins[v:GetUserGroup()] then continue end
 					
 					v:ChatAddText(Color(220, 200, 0), "[BanLister] ", color_white, string.format(BanLister.AdminMessage, name, count))
 				end
 			end
 		end, 
 		function(e)
-			print("[BanLister] API FAILED WITH STATUS: "..e)
+			--print("[BanLister] API FAILED WITH STATUS: "..e)
 		end
 	)
 end)
