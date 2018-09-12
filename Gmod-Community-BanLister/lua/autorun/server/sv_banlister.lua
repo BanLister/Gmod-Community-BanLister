a12409add89ac173144ac09ee278564536cca5e0
Charlie <charliesimon02@gmail.com>
2018-09-12 13:25:05 +1000

    Minor Improvements

--- Gmod-Community-BanLister/lua/autorun/server/sv_banlister.lua
+++ Gmod-Community-BanLister/lua/autorun/server/sv_banlister.lua
@@ -5,9 +5,12 @@ BanLister.MaxBans = 15 -- If the user has equal to or more recorded bans then th
 BanLister.RangeBans = "month" -- "total" - "month" -- MaxBans Time period.
 BanLister.KickHim = false -- if false, instead of kicking users for MaxBans a message will be sent to staff. If true a message won't be sent and the user will be kicked.
 BanLister.KickReason = "Sorry %s but this server is protected by Ban Lister ensuring a safe community" -- %s = player name
-BanLister.allowedAdmins = {["moderator"] = true, ["mod"] = true} -- alert other ranks that may now be classed as Admin with CAMI admin mods.
 BanLister.AdminMessage = "%s has %d bans in the past month recorded" -- %s = player name
-
+BanLister.Debug = false -- turn to true to see debug info in console on player join
+BanLister.allowedAdmins = { -- alert other ranks that may not be classed as Admin with CAMI admin mods.
+	["moderator"] = true,
+	["mod"] = true
+}
 --DO NOT MODIFY PAST THIS POINT--
 
 function BanLister:BanHandler(steamid64, reason, length)
@@ -22,7 +25,7 @@ function BanLister:BanHandler(steamid64, reason, length)
 		},
 		function(result)
 			--print(result)
-		end, 
+		end,
 		function(e)
 			--print("[BanLister] API FAILED WITH STATUS: "..e)
 		end
@@ -30,20 +33,21 @@ function BanLister:BanHandler(steamid64, reason, length)
 end
 
 hook.Add("PlayerInitialSpawn", "BanLister.CheckForBans", function(ply)
-	local steamid64 = ply:SteamID64() 
+	local steamid64 = ply:SteamID64()
 
 	local s = BanLister.RangeBans == "month" and "retrieve-month" or "retrieve"
 
 	http.Fetch(
-		string.format("https://api.banlister.com/%s.php?steamid=%s", s, steamid64), 
+		string.format("https://api.banlister.com/%s.php?steamid=%s", s, steamid64),
 		function(body, size, h, code)
 			local data = util.JSONToTable(body)
 
 			local count = #data -- use the length operator when its numerically indexed (i believe in this case this is). table.Count is good for sequentially
 
-			--PrintTable(data)
-
-			--print("Bans count: "..count)
+			if BanLister.Debug then
+				PrintTable(data)
+				print("Bans count: "..count)
+			end
 
 			if count >= BanLister.MaxBans then return end
 
@@ -52,14 +56,14 @@ hook.Add("PlayerInitialSpawn", "BanLister.CheckForBans", function(ply)
 				ply:Kick(string.format(BanLister.KickReason, name))
 			else
 				for k,v in pairs(player.GetAll()) do
-					if not v:IsAdmin() || not BanLister.allowedAdmins[v:GetUserGroup()] then continue end
-					
+					if not v:IsAdmin() or not BanLister.allowedAdmins[v:GetUserGroup()] then continue end
+
 					v:ChatAddText(Color(220, 200, 0), "[BanLister] ", color_white, string.format(BanLister.AdminMessage, name, count))
 				end
 			end
-		end, 
+		end,
 		function(e)
-			--print("[BanLister] API FAILED WITH STATUS: "..e)
+			print("[BanLister] API FAILED WITH STATUS: "..e)
 		end
 	)
 end)
@@ -80,6 +84,6 @@ end
 
 hook.Add("FAdmin_StoreBan", "BanLister.FAdminHandler", function(steamid, _, length, reason)
 	local steamid64 = util.SteamIDTo64(steamid)
-	
+
 	BanLister:BanHandler(steamid64, reason, length)
 end)
--- README.md
+++ README.md
@@ -12,17 +12,21 @@ If your looking to get a Community Key, contact ```John Snow Gum``` https://John
 
 Download the ZIP file and extract ```Gmod-Community-BanLister``` addon to the addons folder.
 
-### Configuration 
+### Configuration
 
-Open ```sv_banlister.lua``` from the ```lua/autorun/server``` directory and modify the following fields as follows. 
+Open ```sv_banlister.lua``` from the ```lua/autorun/server``` directory and modify the following fields as follows.
 ```
 BanLister.API = "" -- Provided BanLister Community Key
 BanLister.MaxBans = 15 -- If the user has equal to or more recorded bans then this value, then the user will be kicked from the server.
 BanLister.RangeBans = "month" -- "total" - "month" -- MaxBans Time period.
 BanLister.KickHim = false -- if false, instead of kicking users for MaxBans a message will be sent to staff. If true a message won't be sent and the user will be kicked.
 BanLister.KickReason = "Sorry %s but this server is protected by Ban Lister ensuring a safe community" -- %s = player name
-BanLister.allowedAdmins = {["moderator"] = true, ["mod"] = true} -- alert other ranks that may not be classed as Admin with CAMI admin mods.
 BanLister.AdminMessage = "%s has %d bans in the past month recorded" -- %s = player name
+BanLister.Debug = false -- turn to true to see debug info in console on player join
+BanLister.allowedAdmins = { -- alert other ranks that may not be classed as Admin with CAMI admin mods.
+	["moderator"] = true,
+	["mod"] = true
+}
 ```
 
 ### Security
